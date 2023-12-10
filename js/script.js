b40c939e0f40756bd64cf3a984f6c650080d23cd
DoKe65 <aurelia65@gmail.com>
2023-12-10 17:17:55 +0100

    Added quotes array

--- js/script.js
+++ js/script.js
@@ -3,14 +3,42 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * `quotes` array
 ***/
+const quotes = [
+  {
+    quote: "Confusion is part of programming.",
+    source: "Felienne Hermans",
+    citation: "The Programmer's Brain",
+    year: 2021
+  },
+  {
+    quote: "My mother always used to say: The older you get, the better you get - unless you're a banana.",
+    source: "Rose (Betty White)",
+    citation: "The Golden Girls"
+  },
+  {
+    quote: "Common sense is like deodorant. The people who need ist most never use it.",
+    source: "Mackenzie Morrison"
+  },
+  {
+    quote: "There is only one thing more painful than learning from experience and that is not learning from experience.",
+    source: "Archibald McLeish"
+  },
+  {
+    quote: "In youth we learn; in age we understand.",
+    source: "Marie von Ebner-Eschenbach"
+  },
+  {
+    quote: "Do not fear failure but rather fear not trying.",
+    source: "Roy T. Bennett",
+    citation: "The Light in the Heart",
+    year: 2016
+  }
 
+];
 
 
 /***
