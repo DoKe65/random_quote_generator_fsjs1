2c74ccdf25c5026db4fdee540bbb5745d2beb7ba
DoKe65 <aurelia65@gmail.com>
2023-12-17 18:48:54 +0100

    Added exceeds requirements

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 by Doris Keller
 
 This is the first project for the [Treehouse](https://www.teamtreehouse.com) Full Stack JavaScript Techdegree. 
+## Languages and Techniques
+To create this project I used the following languages:
+- JavaScript
+- CSS
+
+The html and most of the css was already provided.
+
+## Tested on:
+- Firefox
+- Chrome
+- Safari
+- Edge
+
+**Be happy and code on!**
--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ project 1 - A Random Quote Generator
  * Constants with urls where I found some of the quotes
  */
 
- const goodreads = `<a href="https://www.goodreads.com/quotes" target="_blank">(goodreads.com)</a>`;
- const stacker = `<a href="https://stacker.com/stories/2617/can-you-name-who-said-these-50-famous-quotes" target="_blank">(stacker.com)</a>`;
- const brainy = `<a href="https://www.brainyquote.com/quotes" target="_blank">(brainyquote.com)</a>`
+ const goodreads = `<a href="https://www.goodreads.com/quotes" target="_blank">goodreads.com</a>`;
+ const stacker = `<a href="https://stacker.com/stories/2617/can-you-name-who-said-these-50-famous-quotes" target="_blank">stacker.com</a>`;
+ const brainy = `<a href="https://www.brainyquote.com/quotes" target="_blank">brainyquote.com</a>`
  
 
 /*** 
@@ -30,7 +30,7 @@ const quotes = [
     resource: goodreads
   },
   {
-    quote: "Common sense is like deodorant. The people who need ist most never use it.",
+    quote: "Common sense is like deodorant. The people who need it most never use it.",
     source: "Mackenzie Morrison",
     resource: goodreads
   },
@@ -76,35 +76,84 @@ const quotes = [
   }
 ];
 
+/***
+ * Array "indexes" to store the last 7 quote indexes
+ */
+
+let indexes = [];
 
 /***
- * `getRandomQuote` function
+ * @function getRandomQuote(arr)
  * @param {array} arr - The array to pass to the function
- * @returns {object} - Returns the object at the randomly generated index from the array passed to the function 
+ * @returns {object} - Returns the randomly chosen quote object 
+ * @description: 
+ * 1. stores index in a variable
+ * 2. generates a random number from 0 to the length of the quotes array, if not already used and stored in the indexes array
+ * 3. adds the current generated random value into the indexes array
+ * 4. When array contains 7 indexes, reset (empty) array "indexes"
+ * 5. returns the quote object
  */
 const getRandomQuote = arr => {
-  const index = Math.floor(Math.random() * arr.length);
+  let index;
+
+  do {
+    index = Math.floor(Math.random() * arr.length);
+  }
+  while ( indexes.includes(index) );
+  indexes.push(index);
+  
+  if (indexes.length = 7) {
+    indexes = [];
+  }
+
   return arr[index];
 }
 
-console.log(getRandomQuote(quotes))
+/***
+ * @function randomBg()
+ * @returns {string} color - creates and returns a random rgb color
+ */
+
+const randomBg = () => {
+  const val = () => Math.floor(Math.random() * 256);
+  return `rgb(${val()}, ${val()}, ${val()})`;
+}
+
 
 
 /***
- * `printQuote` function
+ * @function printQuote()
+ * @returns {string} - Returns html string containing quote object returned from the getRandomQuote function
+ * @description:
+ * 1. Generates an html string to display the randomly chosen quote by the getRandomQuote() function
+ * 2. Adds the html to the quote-box div
+ * 3. Sets background color to a random color using the function randomBg()
 ***/
 
-/***
- * Prevent previously displayed quotes from being displayed again
- * Store current index in array
- * test current random index against content of array
- * only display, if index was not used for the last 6 attempts. 
- * When the array of used indexes reaches the number of 7, empty the array
+const printQuote = () => {
+  const {quote, source, citation, year, resource} = getRandomQuote(quotes);
+  let html = `
+    <p class="quote">${quote}</p>
+    <p class="source">${source}`;
+   citation ? html += `<span class="citation">${citation}</span>` : null;
+   year ? html += `<span class="year">${year}</span>` : null;
+   resource ? html += `<span class="resource">found on ${resource}</span>` : null;
+   html += `</p>`;
+   document.querySelector("#quote-box").innerHTML = html;
+   document.querySelector("body").style.backgroundColor = randomBg();
+ }
+
+printQuote();
+
+/**
+ * Displays a new quote every 10 seconds
  */
+
+setInterval(printQuote, 10000)
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-//document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
